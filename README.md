da9391e9b3bf1239700aad03f5ce4f78f5ff6922
Francisco Del Roio <francipvb@hotmail.com>
2018-07-03 10:26:20 -0300

    Agregando archivos iniciales.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# RLMush
-Scripts to play reinos de leyenda with mushclient.
+# Plugins de reinos de leyenda
+
+Estos archivos contienen scripts que facilitan jugar a reinos de leyenda bajo mushclient.
+
+## Instalación
+
+1. Descargar el repositorio, bien mediante git, o bien mediante [este enlace](https://github.com/francipvb/RLMush/archive/master.zip).
+2. Extraer los archivos en la carpeta raíz de MUSHClient.
+3. Abrir MUSHClient y generar un nuevo mundo.
+4. Pulsar CTRL+SHIFT+p y agregar todos los plugins con el prefijo rl, además del plugin luaaudio.
+5. Entrar con una ficha y habilitar el GMCP tecleando `consentir gmcp on`.
+
+Tras esto, ya se puede jugar normalmente, aunque hay un paso específico todavía.
+
+Para evitar que el lector lea cosas muy extrañas es recomendable poner el MXP en yes always desde Game -> Configure -> MXP/Pueblo.
+
+Puede que la última línea no se lea correctamente. Para arreglarlo, ir a Game -> Configure -> Output y marcar la casilla _Convert IAC EOR/GA to new line_.
+
+## Uso del mapa (fase alpha)
+
+De momento, el plugin no es capaz de generar datos de mapa, pero sí de importarlos y leerlos. Para configurar el mapa, se necesita el archivo de mapa de zmud.
+
+Tras descargar y descomprimir el archivo de mapa de ZMud:
+
+1. Teclear `mapa importar` y seleccionar el archivo de mapa (*.mdb) descargado.
+2. Esperar hasta que la importación se complete.
+3. Caminar normalmente hasta una sala identificable.
+
+Y eso es todo.
+
+Para algunos comandos básicos, teclear mapa ayuda.
+
+### Posibles problemas
+
+Hay un problema con la funcionalidad nativa de listas en mushclient por lo que no es posible utilizar la lista para seleccionar un destino al navegar. El único modo es usar los enlaces que el mapper genera en la ventana de salida, al menos de momento. Cuando pueda ponerme en contacto con el autor de MUSHClient para agregar esta funcionalidad voy a cambiar esta parte del plugin.
+
+Por cualquier inconveniente, reportar generando una issue.
---
+++ lua/rl_stuff.lua
@@ -0,0 +1,138 @@
+require("ppi")
+local snd = nil
+local gmcp = nil
+
+-- Funciones asociadas a mensajes enviados mediante GMCP
+local suscriptores = {}
+
+mushreader = nil
+
+local html_replacements = {
+	['&lt;'] = "<",
+	['&gt;'] = ">",
+	['&quot;'] = '"'
+}
+
+local coros = {}
+
+-- Tomado de la wiki de LUA
+local function interp(s, tab)
+	return (s:gsub('($%b{})', function(w) return tab[w:sub(3, -2)] or w end))
+end
+getmetatable("").__mod = interp
+
+
+function replaceHTMLEscape(s)
+	return html_replacements[s]
+end -- function
+
+function RLStuffListChanged()
+	mushreader = ppi.Load("16ba6b7a227a9dab54f5c76e")
+	snd = ppi.Load("aedf0cb0be5bf045860d54b7")
+	gmcp = ppi.Load("3b764c2d9d7e85dbf01ddfab")
+end -- OnPluginListChanged
+
+function playSound(sound,loop,pan,vol)
+	if not snd then
+		return
+	end -- if
+	return snd.play(GetInfo(67)..sound,loop,pan,vol)
+end -- PlaySound
+
+function playDelay(sound,time,pan,vol)
+	snd.playDelay(GetInfo(67).."sounds/"..sound,time,pan,vol)
+end -- PlayDelay
+
+function playListOfSounds(list, time, pan, vol)
+	-- Comprobación de errores
+	assert(list, "No se pasó una lista de sonidos.")
+	assert(type(list) == "table", string.format("Se pasó un %s, se esperaba una tabla.", type(list)))
+	
+	for  k, v in pairs(list) do
+		playDelay(v, k*time, pan, vol)
+	end
+end -- playListOfSounds
+
+function slidePan(x,id,time)
+	if not snd then
+		snd = ppi.Load("aedf0cb0be5bf045860d54b7")
+	end -- snd
+	return snd.slidePan(x,id,time)
+end -- SlidePan
+
+function utf8convert(str)
+	local data = utils.utf8decode(str)
+	local retval = ""
+	
+	if not data then
+		return str
+	end -- if
+	
+	for k, v in pairs(data) do
+		local character = ""
+		local res = false
+		res, character = pcall(string.char, v)
+		if res then
+			retval = retval..character
+		else
+			retval = retval.."?"
+		end -- if
+	end -- data
+	
+	return retval
+end -- utf8convert
+
+function history_add(cat, msg)
+	CallPlugin("f6153a4ac099403418c11711", "history_add", cat, msg)
+end -- add_history
+
+function play_sound(filepath, pan)
+	local folder = "sounds/" .. filepath
+
+	return playSound(folder, 0, pan or 0)
+end -- play_sound
+
+function subscribir(mensaje, funcion)
+	assert(mensaje, "No se definió un mensaje.")
+	assert(type(mensaje) == "string", "El mensaje no es válido.")
+	assert(funcion, "No se suplió una función.")
+	assert(type(funcion) == "string", "Solo se admite el nombre de la función.")
+	
+	if gmcp then
+		gmcp.add_callback(mensaje, GetPluginID(), funcion)
+	end 
+end -- function
+
+function desuscribir(mensaje, funcion)
+	if gmcp then
+		gmcp.remove_callback(mensaje, GetPluginID(), funcion)
+	end
+end -- function
+
+function OnPluginTick()
+	for k, v in pairs(coros) do
+		if not completo(k) then
+			local success, r1, r2, r3 = coroutine.resume(v)
+			if success then
+				if type(r1) == "string" then
+					print(r1)
+				end -- if
+			end -- if
+		else
+			coros[k] = nil
+		end -- if
+	end -- for
+end -- function
+
+function completo(nombre)
+	if not coros[nombre] then
+		return true
+	else
+		local success, result = pcall(coroutine.status, coros[nombre])
+		return success and result == "dead"
+	end -- if
+end -- function
+
+function agregarCoro(nombre, funcion)
+	coros[nombre] = coroutine.create(funcion)
+end -- function
